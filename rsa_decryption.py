a6f6fcfffea70f7a30068a818a4da9047b6189aa
softwarejr <richardjr.magsalay@g.msuiit.edu.ph>
2022-04-01 16:41:16 +0800

    Format code based on assignment convention.

--- rsa_decryption.py
+++ rsa_decryption.py
@@ -1,8 +1,7 @@
-from email import message
-
-
 alphabet = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ.!?,"
-encrypted_message = [
+# c is a list of encrypted interger message
+#each item in the list represents single character
+c = [
     14203,
     13300,
     11291,
@@ -31,42 +30,35 @@ encrypted_message = [
     13300,
     13501,
 ]
-"""
-k = (e,n) such that i,n are key pair
-c is the integer message
-public key = (e,n) = (12847, 19177)
-"""
-e = 12847
-n = 19177
-k = (e, n)
 
 
-def rsa(k):
+# public key = (i,n)
+# k = (i,n) such that i,n are key pair
+i = int(input("Input i: "))
+n = int(input("Input n: "))
+k = (i, n)
+
+# rsa decryption algo
+def rsa(k, c):
     decrypted_message = []
-    e = int(k[0])
-    n = int(k[1])
 
-    for character_number in encrypted_message:
-        message = character_number
-        for i in range(1, e):
-            message *= int(character_number)
-        decrypted = message % n
-        decrypted_message.append(decrypted)
+    for character in c:
+        message = character
+        for i in range(1, k[0]):
+            message *= int(character)
+        message = message % k[1]
+        decrypted_message.append(message)
     return decrypted_message
 
 
-def index_of_c(m, alphabet):
-    message = []
-    for text in m:
-        text_character = alphabet[text]
-        message.append(text_character)
+# covert decrypted integer message into text
+def c2i(m, alphabet):
+    message = ""
+    for i in m:
+        c = alphabet[i]
+        message += c
     return message
 
 
-# print(index_of_c(encrypted_message, alphabet))
-# print(index_of_c(rsa(k), alphabet))
-
-print(len(rsa(k)))
-print(len(encrypted_message))
-print(index_of_c(rsa(k), alphabet))
-
+if __name__ == "__main__":
+    print(c2i(rsa(k, c), alphabet))
